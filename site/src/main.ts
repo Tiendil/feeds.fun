6a1329626955988d2630e247348572a3f94a96a1
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-05 22:27:34 +0200

    components for tags ui

--- site/src/main.ts
+++ site/src/main.ts
@@ -6,19 +6,23 @@ import router from './router'
 
 import FeedsList from "./components/FeedsList.vue";
 import EntriesList from "./components/EntriesList.vue";
+import TagsList from "./components/TagsList.vue";
 
 import ValueUrl from "./values/URL.vue";
 import ValueFeedId from "./values/FeedId.vue";
 import ValueDateTime from "./values/DateTime.vue";
+import ValueTag from "./values/Tag.vue";
 
 const app = createApp(App)
 
 app.component("FeedsList", FeedsList);
 app.component("EntriesList", EntriesList);
+app.component("TagsList", TagsList);
 
 app.component("ValueUrl", ValueUrl);
 app.component("ValueFeedId", ValueFeedId);
 app.component("ValueDateTime", ValueDateTime);
+app.component("ValueTag", ValueTag);
 
 app.use(createPinia())
 app.use(router)
