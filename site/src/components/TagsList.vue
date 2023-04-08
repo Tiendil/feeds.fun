ef6ad36f9f57af5263bdb524bf1bf192aafaa1e2
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-08 15:23:14 +0200

    better tags visualisation

--- site/src/components/TagsList.vue
+++ site/src/components/TagsList.vue
@@ -1,16 +1,52 @@
 <template>
 
-  <ul style="list-style-type: none; margin: 0; padding: 0">
-    <li v-for="tag of tags" style="display: inline">
-      {{tag}}&nbsp;
-    </li>
-  </ul>
+<template v-for="tag of displayedTags">
+    <value-tag :value="tag"/>&nbsp;
+</template>
+
+<a href="#" v-if="canShowAll" @click="showAll=true">more</a>
+
+<a href="#" v-if="canHide" @click="showAll=false">hide</a>
 
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from "vue";
 
-defineProps<{ tags: string[]}>();
+const showAll = ref(false);
+const showLimit = ref(5);
+
+const properties = defineProps<{ tags: string[]}>();
+
+const displayedTags = computed(() => {
+    if (showAll.value) {
+        return preparedTags.value;
+    }
+
+    return preparedTags.value.slice(0, showLimit.value);
+});
+
+const preparedTags = computed(() => {
+    const values = [];
+
+    for (const tag of properties.tags) {
+        const [type_, value] = tag.split(":");
+        values.push(value);
+    }
+
+    values.sort();
+
+    return values;
+});
+
+const canShowAll = computed(() => {
+    return !showAll.value && showLimit.value < preparedTags.value.length;
+});
+
+const canHide = computed(() => {
+    return showAll.value && showLimit.value < preparedTags.value.length;
+});
+
 
 </script>
 
