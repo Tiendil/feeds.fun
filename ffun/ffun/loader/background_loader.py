fa06e6ae483fbaf7c5cc56faa809c15fe13fffef
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-10 14:26:39 +0200

    concurency

--- ffun/ffun/loader/background_loader.py
+++ ffun/ffun/loader/background_loader.py
@@ -1,10 +1,9 @@
-
+import asyncio
 import datetime
 import logging
 
 from ffun.core.background_tasks import InfiniteTask
 from ffun.feeds import domain as f_domain
-from ffun.library import domain as l_domain
 from ffun.loader import domain
 from ffun.loader.settings import settings
 
@@ -14,7 +13,8 @@ logger = logging.getLogger(__name__)
 class FeedsLoader(InfiniteTask):
     __slots__ = ('_loaders_number',)
 
-    def __init__(self, loaders_number: int = 1, **kwargs) -> None:
+    def __init__(self, loaders_number: int = settings.loaders_number,
+                 **kwargs) -> None:
         super().__init__(**kwargs)
         self._loaders_number = loaders_number
 
@@ -22,6 +22,6 @@ class FeedsLoader(InfiniteTask):
         feeds = await f_domain.get_next_feeds_to_load(number=self._loaders_number,
                                                       loaded_before=datetime.datetime.utcnow() - settings.minimum_period)
 
-        # TODO: run concurrently
-        for feed in feeds:
-            await domain.process_feed(feed)
+        tasks = [domain.process_feed(feed) for feed in feeds]
+
+        await asyncio.gather(*tasks, return_exceptions=True)
