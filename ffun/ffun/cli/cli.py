38bdc6c3007689f851bfd2e2b5b28d818e6a4d76
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-11 19:34:34 +0200

    removed load feed command

--- ffun/ffun/cli/cli.py
+++ ffun/ffun/cli/cli.py
@@ -1,6 +1,5 @@
 from .application import app  # noqa: F401
 from .commands import discover_tags  # noqa: F401
-from .commands import load_feeds  # noqa: F401
 from .commands import load_opml  # noqa: F401
 from .commands import server  # noqa: F401
 
