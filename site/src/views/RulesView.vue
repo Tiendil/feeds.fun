72f49f130869227e8aaafb733af4ae6b1d1dd19f
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-19 19:55:10 +0200

    fixed chaning of mainPanelMode

--- site/src/views/RulesView.vue
+++ site/src/views/RulesView.vue
@@ -5,8 +5,14 @@
 <script lang="ts" setup>
 import { computed, ref, onUnmounted, watch } from "vue";
 import { computedAsync } from "@vueuse/core";
+import { useGlobalSettingsStore } from "@/stores/globalSettings";
 import * as api from "@/logic/api";
 import * as t from "@/logic/types";
+import * as e from "@/logic/enums";
+
+const globalSettings = useGlobalSettingsStore();
+
+globalSettings.mainPanelMode = e.MainPanelMode.Rules;
 
 const rules = computedAsync(async () => {
     return await api.getRules();
