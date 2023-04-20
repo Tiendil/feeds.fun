aae49967c0f1af4f06d7de2244a23baef7aaaac3
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-20 18:46:37 +0200

    more concurency for openai processor

--- ffun/ffun/librarian/background_processors.py
+++ ffun/ffun/librarian/background_processors.py
@@ -36,7 +36,7 @@ class ProcessorInfo:
 
 processors = [ProcessorInfo(id=1, processor=DomainProcessor(name='domain'), concurrency=1),
               ProcessorInfo(id=2, processor=NativeTagsProcessor(name='native_tags'), concurrency=1),
-              ProcessorInfo(id=3, processor=OpenAIChat35Processor(name='openai_chat_3_5'), concurrency=10)]
+              ProcessorInfo(id=3, processor=OpenAIChat35Processor(name='openai_chat_3_5'), concurrency=25)]
 
 
 class EntriesProcessor(InfiniteTask):
