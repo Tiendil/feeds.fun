fe03b3d9798352f03acf03fc572b83a8ec0e9545
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-04-28 20:01:06 +0200

    better feeds list visualization

---
+++ site/src/components/FeedForList.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="container">
+
+  <div style="flex-shrink: 0; width: 1rem; left-padding: 0.25rem;">
+    <value-date-time :value="feed.loadedAt" :reversed="true"/>
+  </div>
+
+  <div style="flex-shrink: 0; width: 2rem; text-align: right; padding-right: 0.25rem;">
+    <span v-if="isOk"
+          title="everything is ok"
+          class="state-ok">ok</span>
+    <span v-else
+          :title="feed.lastError"
+          class="state-error">⚠</span>
+  </div>
+
+  <div style="flex-grow: 1;">
+    <value-url :value="feed.url"
+               :text="purifiedTitle"/>
+    <br/>
+    <div v-html="purifiedDescription"/>
+  </div>
+
+</div>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import * as t from "@/logic/types";
+import * as e from "@/logic/enums";
+import { computedAsync } from "@vueuse/core";
+import DOMPurify from "dompurify";
+import { useEntriesStore } from "@/stores/entries";
+
+const entriesStore = useEntriesStore();
+
+const properties = defineProps<{ feed: t.Feed}>();
+
+const purifiedTitle = computed(() => {
+    if (properties.feed.title === null) {
+        return "";
+    }
+
+    let title = DOMPurify.sanitize(properties.feed.title, {ALLOWED_TAGS: []});
+
+    if (title.length === 0) {
+        return null;
+    }
+
+    return title;
+});
+
+const purifiedDescription = computed(() => {
+    if (properties.feed.description === null) {
+        return "";
+    }
+    return DOMPurify.sanitize(properties.feed.description);
+});
+
+const isOk = computed(() => {
+    return properties.feed.state === "loaded";
+});
+
+</script>
+
+<style scoped>
+  .container {
+      display: flex;
+  }
+
+  .container :deep(img) {
+     max-width: 100%;
+     height: auto;
+  }
+
+  .state-ok {
+    color: green;
+    cursor: default;
+}
+
+  .state-error {
+      color: red;
+      cursor: default;
+  }
+</style>
