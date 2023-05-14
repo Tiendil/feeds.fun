c41ceb4bc3c4f6d7421628e43d96c166c4c6fd6b
Aliaksei Yaletski <a.eletsky@gmail.com>
2023-05-14 14:01:20 +0200

    tracking last state of processed marker & reprocessing in case of troubles (#11)

---
+++ ffun/ffun/library/migrations/20230514_01_Bwb35-processed-state.py
@@ -0,0 +1,22 @@
+"""
+processed-state
+"""
+
+from yoyo import step
+
+__depends__ = {'20230427_01_pv33u-fix-entries-unique-index'}
+
+
+def apply_step(conn):
+    cursor = conn.cursor()
+    cursor.execute('ALTER TABLE l_entry_process_info ADD COLUMN state SMALLINT NOT NULL DEFAULT 1')
+    cursor.execute('ALTER TABLE l_entry_process_info ADD last_error TEXT')
+
+
+def rollback_step(conn):
+    cursor = conn.cursor()
+    cursor.execute("ALTER TABLE l_entry_process_info DROP COLUMN state")
+    cursor.execute("ALTER TABLE l_entry_process_info DROP COLUMN last_error")
+
+
+steps = [step(apply_step, rollback_step)]
