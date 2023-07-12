bf3967ee60da3b0c37af887e0c8a52b42adcad72
Aliaksei Yaletski <a.eletsky@gmail.com>
2023-07-12 18:10:16 +0200

    gh-73 make HTTP requests via proxy (#75)

--- ffun/ffun/loader/settings.py
+++ ffun/ffun/loader/settings.py
@@ -1,11 +1,18 @@
 import datetime
 
+import pydantic
 from ffun.core.settings import BaseSettings
+
+
+class Proxy(pydantic.BaseModel):
+    name: str
+    url: str|None = None
 
 
 class Settings(BaseSettings):  # type: ignore
     loaders_number: int = 5
-    minimum_period: datetime.timedelta = datetime.timedelta(minutes=10)
+    minimum_period: datetime.timedelta = datetime.timedelta(hours=1)
+    proxies: list[Proxy] = [Proxy(name='default', url=None)]
 
     class Config:
         env_prefix = "FFUN_LOADER_"
