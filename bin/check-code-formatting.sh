8b324082b74bd4e71909f47eae095cf424fb89e9
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-07-19 23:13:48 +0200

    some logging

--- bin/check-code-formatting.sh
+++ bin/check-code-formatting.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+echo "run isort"
+
 ./bin/backend-utils.sh poetry run isort --check-only .
+
+echo "run black"
+
 ./bin/backend-utils.sh poetry run black --check .
--- bin/format-code.sh
+++ bin/format-code.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 
+echo "run isort"
+
 ./bin/backend-utils.sh poetry run isort .
+
+echo "run black"
+
 ./bin/backend-utils.sh poetry run black .
