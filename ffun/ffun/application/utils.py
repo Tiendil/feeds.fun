4f1f99754c180b2d7412b14f945a658601fd92c6
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-07-22 20:57:46 +0200

    user_agent for loader

---
+++ ffun/ffun/application/utils.py
@@ -0,0 +1,12 @@
+
+import pkg_resources
+
+import ffun
+
+from .settings import settings
+
+
+def user_agent():
+    name = settings.app_name.replace(' ', '')
+    version = pkg_resources.get_distribution("ffun").version
+    return f'{name}/{version} ({settings.environment} {settings.app_domain})'
