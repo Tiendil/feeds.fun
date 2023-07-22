039b55372b55be19c2242795c9dc78233606ddd4
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-07-22 22:07:03 +0200

    fixed types for adding feeds api

--- ffun/ffun/api/http_handlers.py
+++ ffun/ffun/api/http_handlers.py
@@ -19,6 +19,7 @@ from ffun.library import entities as l_entities
 from ffun.markers import domain as m_domain
 from ffun.ontology import domain as o_domain
 from ffun.parsers import domain as p_domain
+from ffun.parsers import entities as p_entities
 from ffun.resources import domain as r_domain
 from ffun.scores import domain as s_domain
 from ffun.scores import entities as s_entities
@@ -205,7 +206,7 @@ async def api_discover_feeds(request: entities.DiscoverFeedsRequest, user: User)
     return entities.DiscoverFeedsResponse(feeds=external_feeds)
 
 
-async def _add_feeds(feed_infos: list[entities.FeedInfo], user: User) -> None:
+async def _add_feeds(feed_infos: list[p_entities.FeedInfo], user: User) -> None:
     feeds = [
         f_entities.Feed(id=uuid.uuid4(), url=feed_info.url, title=feed_info.title, description=feed_info.description)
         for feed_info in feed_infos
--- ffun/ffun/parsers/domain.py
+++ ffun/ffun/parsers/domain.py
@@ -1,10 +1,11 @@
 from ffun.feeds.entities import Feed
 
 from . import feed
+from .entities import FeedInfo
 from .feedly import extract_feeds
 
 
-def parse_opml(data: str) -> list[Feed]:
+def parse_opml(data: str) -> list[FeedInfo]:
     return extract_feeds(data)
 
 
