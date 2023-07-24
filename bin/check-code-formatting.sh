b752b9c563ecbc3f349ef330e72eb58890b39d1a
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-07-24 20:42:07 +0200

    rollback debug prints

--- bin/check-code-formatting.sh
+++ bin/check-code-formatting.sh
@@ -10,10 +10,6 @@ echo "run black"
 
 ./bin/backend-utils.sh poetry run black --check .
 
-echo "run prettier!!!"
-
-./bin/frontend-utils.sh ls -lah
-
-echo "????"
+echo "run prettier"
 
 ./bin/frontend-utils.sh npm run format-check
