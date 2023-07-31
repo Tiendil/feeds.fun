16b375d8a96b268e6e319a2d82086720ffb20bcb
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-07-31 20:34:33 +0200

    format code

--- ffun/ffun/core/settings.py
+++ ffun/ffun/core/settings.py
@@ -3,8 +3,10 @@ import pydantic_settings
 
 
 class BaseSettings(pydantic_settings.BaseSettings):
-    model_config = pydantic_settings.SettingsConfigDict(env_nested_delimiter="__",
-                                                        # TODO: smart search for .env file
-                                                        # or update to pydantic-settings when it will be released
-                                                        env_file=("../.env", ".env"),
-                                                        extra="allow")
+    model_config = pydantic_settings.SettingsConfigDict(
+        env_nested_delimiter="__",
+        # TODO: smart search for .env file
+        # or update to pydantic-settings when it will be released
+        env_file=("../.env", ".env"),
+        extra="allow",
+    )
