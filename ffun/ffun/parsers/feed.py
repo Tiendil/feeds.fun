0a47cdad01750c7dea898b3e50fbe4a97e4e2fae
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-10-07 12:13:40 +0200

    domain module

--- ffun/ffun/parsers/feed.py
+++ ffun/ffun/parsers/feed.py
@@ -2,9 +2,9 @@ import datetime
 from typing import Any, Iterable
 
 import feedparser
-from furl import furl
 
 from ffun.core import logging
+from ffun.domain import urls
 from ffun.parsers.entities import EntryInfo, FeedInfo
 
 logger = logging.get_module_logger()
@@ -47,41 +47,10 @@ def _extract_external_id(entry: Any) -> str:
     return entry.get("link")
 
 
-# is required for correct parsing by furl
-# will be removed before returning result
-def _fake_schema_for_url(url: str) -> str:
-    if '//' not in url and url[0] != '/' and ('.' in url.split('/')[0]):
-        return f'//{url}'
-
-    return url
-
-
-def _normalize_external_url(url: str, original_url: str) -> str:
-    url = _fake_schema_for_url(url)
-    original_url = _fake_schema_for_url(original_url)
-
-    external_url = furl(url)
-
-    f_original_url = furl(original_url)
-
-    if not external_url.scheme:
-        external_url.set(scheme=f_original_url.scheme)
-
-    if not external_url.netloc:
-        external_url.set(netloc=f_original_url.netloc)
-
-    result_url = str(external_url)
-
-    if result_url.startswith('//'):
-        result_url = result_url[2:]
-
-    return result_url
-
-
 def _extract_external_url(entry: Any, original_url: str) -> str:
     url = entry.get("link")
 
-    return _normalize_external_url(url, original_url)
+    return urls._normalize_external_url(url, original_url)
 
 
 def parse_feed(content: str, original_url: str) -> FeedInfo | None:
