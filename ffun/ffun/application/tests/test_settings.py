745eb393d4f99360c501a1144aca11cc0aa48a97
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-10-08 17:38:35 +0200

    types

--- ffun/ffun/application/tests/test_settings.py
+++ ffun/ffun/application/tests/test_settings.py
@@ -4,10 +4,10 @@ from ffun.application.settings import Settings
 
 
 class TestSettings:
-    def test_origin_must_be_redefined_in_prod(self):
+    def test_origin_must_be_redefined_in_prod(self) -> None:
         with pytest.raises(ValueError):
             Settings(environment="prod")
 
-        settings = Settings(environment="prod", origins=["http://localhost:3000", "http://localhost:3001"])
+        settings = Settings(environment="prod", origins=("http://localhost:3000", "http://localhost:3001"))
 
         assert settings.origins == ("http://localhost:3000", "http://localhost:3001")
