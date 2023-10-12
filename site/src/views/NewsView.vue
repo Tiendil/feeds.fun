0be2a524cac5c034ab630fff5446da6b2ca5461d
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-10-12 23:53:55 +0200

    better info panels

--- site/src/views/NewsView.vue
+++ site/src/views/NewsView.vue
@@ -46,7 +46,7 @@
     <template #main-footer> </template>
 
     <div v-if="!hasEntries && !entriesStore.firstTimeEntriesLoading"
-         class="ffun-info-normal">
+         class="ffun-info-attention">
       <p>It looks like you have no news to read.</p>
       <p> Try to subscribe for the feeds collections that we are preparing for you! </p>
       <feeds-collections />
