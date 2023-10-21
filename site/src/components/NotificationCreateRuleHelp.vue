c7956f3fa6b8c83068478234907183406b33f4e3
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2023-10-21 12:59:00 +0200

    better notification

--- site/src/components/NotificationCreateRuleHelp.vue
+++ site/src/components/NotificationCreateRuleHelp.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="ffun-info-attention">
     <p>
-      Create your first rule to see the full power of the reader!
+      Make your first rule to experience the reader's full capabilities!
     </p>
 
-    <ul class="list-decimal">
-      <li>Click on one of the tags.</li>
-      <li>Select more tags if required.</li>
-      <li>Choose score for the rule.</li>
-      <li>Click on the "Create Rule" button.</li>
-      <li>Look how news are sorted according your wishes.</li>
+    <ul class="list-decimal list-inside">
+      <li>Click a tag under a news.</li>
+      <li>Select more tags if needed.</li>
+      <li>Set a score for the rule.</li>
+      <li>Click "Create Rule".</li>
+      <li>See how the news gets organized based on your preferences!</li>
     </ul>
 
   </div>
