3959f7db9acaa38945650f1dcc9f8983fdde193d
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-03-24 15:11:59 +0100

    better exceptions formatting

--- ffun/ffun/core/errors.py
+++ ffun/ffun/core/errors.py
@@ -10,7 +10,7 @@ class Error(Exception):
         if "fingerprint" not in kwargs:
             self.fingerprint = None
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         attributes = ", ".join(f"{key}={value}" for key, value in self.__dict__.items())
 
         return f"{self.__class__.__name__}: {attributes}"
