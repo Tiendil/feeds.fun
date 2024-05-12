0a18ad990f667263ca154386e61ee4142ccd0764
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-05-12 14:03:29 +0200

    migration boilerplate

--- ffun/ffun/cli/cli.py
+++ ffun/ffun/cli/cli.py
@@ -2,8 +2,10 @@ from ffun.cli.application import app  # noqa: F401
 from ffun.cli.commands import configs  # noqa: F401
 from ffun.cli.commands import entries  # noqa: F401
 from ffun.cli.commands import failed_entries  # noqa: F401
+from ffun.cli.commands import meta  # noqa: F401
 from ffun.cli.commands import supertokens  # noqa: F401
 from ffun.cli.commands import workers  # noqa: F401
 from ffun.cli.commands import yoyo  # noqa: F401
+
 
 __all__ = ["app"]
