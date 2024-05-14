db521d9c4d3c0c53c39114549712321d1cb651a9
Aliaksei Yaletski <a.eletsky@gmail.com>
2024-05-14 11:00:39 +0200

    gh-188 Empty migration with merging duplicated feeds  (#190)
    close gh-188

--- ffun/ffun/feeds/migrations/20240504_02_gEapd-fill-uids-for-feeds.py
+++ ffun/ffun/feeds/migrations/20240504_02_gEapd-fill-uids-for-feeds.py
@@ -17,22 +17,11 @@ def get_feeds(cursor: Any) -> list[dict[str, Any]]:
 
 
 def apply_step(conn: Connection[dict[str, Any]]) -> None:
-    # Generally, we should avoid importing from the main project in migrations
-    # Instead, we should copy the code here to achive consistency of migrations
-    # But for this case it looks fine and, what is more important, faster to import
-    from ffun.domain.urls import url_to_uid
-
-    cursor = conn.cursor()
-
-    for feed in get_feeds(cursor):
-        uid = url_to_uid(feed["url"])
-
-        cursor.execute("UPDATE f_feeds SET uid = %(uid)s WHERE id = %(id)s", {"uid": uid, "id": feed["id"]})
+    pass
 
 
 def rollback_step(conn: Connection[dict[str, Any]]) -> None:
-    cursor = conn.cursor()
-    cursor.execute("UPDATE f_feeds SET uid = NULL")
+    pass
 
 
 steps = [step(apply_step, rollback_step)]
