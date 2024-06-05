22aeebe645fcc184e0c1eba2863e3ef9fed76b65
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-06-05 22:32:54 +0200

    code formatting

--- ffun/ffun/ontology/migrations/20240605_01_PC7sP-not-null-o-tags-properties-tag-id.py
+++ ffun/ffun/ontology/migrations/20240605_01_PC7sP-not-null-o-tags-properties-tag-id.py
@@ -2,26 +2,23 @@
 not-null-o_tags_properties-tag_id
 """
 
-from yoyo import step
-
 from typing import Any
 
 from psycopg import Connection
 from yoyo import step
 
-
-__depends__ = {'20230617_03_de7vS-tags-properties'}
+__depends__ = {"20230617_03_de7vS-tags-properties"}
 
 
 def apply_step(conn: Connection[dict[str, Any]]) -> None:
     cursor = conn.cursor()
 
-    cursor.execute('ALTER TABLE o_tags_properties ALTER COLUMN tag_id SET NOT NULL')
+    cursor.execute("ALTER TABLE o_tags_properties ALTER COLUMN tag_id SET NOT NULL")
 
 
 def rollback_step(conn: Connection[dict[str, Any]]) -> None:
     cursor = conn.cursor()
-    cursor.execute('ALTER TABLE o_tags_properties ALTER COLUMN tag_id DROP NOT NULL')
+    cursor.execute("ALTER TABLE o_tags_properties ALTER COLUMN tag_id DROP NOT NULL")
 
 
 steps = [step(apply_step, rollback_step)]
