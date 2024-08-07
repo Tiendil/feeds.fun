d17c684b0347f1ecbe3863d1fda7f698237c5512
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-08-07 15:36:33 +0200

    9

--- ffun/ffun/processors_quality/entities.py
+++ ffun/ffun/processors_quality/entities.py
@@ -1,4 +1,5 @@
 import datetime
+import pydantic
 import enum
 import uuid
 from typing import Any
@@ -9,3 +10,10 @@ from ffun.core.entities import BaseEntity
 class ExpectedTags(BaseEntity):
     must_have: set[str]
     should_have: set[str]
+
+    @pydantic.model_validator(mode='after')
+    def tags_must_not_intersect(self) -> 'ExpectedTags':
+        if self.must_have & self.should_have:
+            raise ValueError(f'Tags must not intersect, common tags: {self.must_have & self.should_have}')
+
+        return self
