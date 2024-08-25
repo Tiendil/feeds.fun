f5ab17e0b64a9b71301ded47b11809831e957ab2
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-08-25 16:14:30 +0200

    feeds.fun collection

--- ffun/ffun/feeds_collections/predefines.py
+++ ffun/ffun/feeds_collections/predefines.py
@@ -1,4 +1,4 @@
-from ffun.feeds_collections.collections import artificial_intelligence, gamedev
+from ffun.feeds_collections.collections import artificial_intelligence, gamedev, feeds_fun
 from ffun.feeds_collections.entities import Collection
 
 # temporary solution to fill feeds for a new user
@@ -14,6 +14,7 @@ from ffun.feeds_collections.entities import Collection
 # TODO: for each source add short "why added" description and show it in UI
 
 predefines = {
+    Collection.feeds_fun: feeds_fun.collection,
     Collection.gamedev: gamedev.collection,
     Collection.artificial_intelligence: artificial_intelligence.collection,
 }
