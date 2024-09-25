91445bd78283a20e41c121fa0fb2541877c2e371
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-09-25 20:52:58 +0200

    fixed tests

--- ffun/ffun/feeds_collections/tests/fixtures.py
+++ ffun/ffun/feeds_collections/tests/fixtures.py
@@ -5,7 +5,7 @@ import pytest_asyncio
 
 from ffun.feeds_collections.collections import collections
 from ffun.feeds_collections.domain import new_collection_id
-from ffun.feeds_collections.entities import Collection, CollectionId
+from ffun.feeds_collections.entities import Collection, CollectionId, FeedInfo
 from ffun.feeds_collections.settings import settings
 
 
@@ -16,8 +16,13 @@ def collection_configs_must_be_none_in_tests() -> None:
 
 @pytest_asyncio.fixture(scope="session")
 async def collection_id_for_test_feeds(collection_configs_must_be_none_in_tests: None) -> CollectionId:
+
+    feed_id_ = uuid.uuid4().hex
+
+    feed = FeedInfo(url=f"http://example.com/{feed_id_}", title=f"Feed {feed_id_}", description=f"Feed {feed_id_}")
+
     collection = Collection(
-        id=new_collection_id(), gui_order=1, name=uuid.uuid4().hex, description=uuid.uuid4().hex, feeds=[]
+        id=new_collection_id(), gui_order=1, name=uuid.uuid4().hex, description=uuid.uuid4().hex, feeds=[feed]
     )
 
     await collections.add_test_collection(collection)
