fea6d5770ad55d4a2c56a05ab366dcb3e110ef14
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-10-10 12:27:09 +0200

    warning text

--- site/src/components/collections/Warning.vue
+++ site/src/components/collections/Warning.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="ffun-info-attention">
-    <h3>
-      Remember about <a
-                       class="ffun-normal-link"
-        href="#"
-        @click.prevent="goToCollections()"
-        >collections</a
-      ></h3>
+    <h3>Check your feed URLs</h3>
+
     <p
-      >!</p
-    >
-    <p>!</p>
+      >Websites like <a href="https://reddit.com" target="_blank">Reddit</a> or <a href="https://arxiv.org" target="_blank">arXiv</a> provide multiple feeds by:</p>
+
+    <ul>
+      <li>Customizing news categories lists in URL (like <pre class="inline-block">https://site.com/feed/rss?category1+category2</pre>) instead of using separate URLs.</li>
+      <li>Supporting different feed formats (like <pre class="inline-block">https://site.com/feed/rss</pre> and <pre class="inline-block">https://site.com/feed/atom</pre>).</li>
+    </ul>
+
+    <p>This can lead to the same news item appearing in multiple feeds, not being recognized as news from collections, and being processed with your API key. We do our best to detect such news, but there are always exceptions.</p>
+
+    <p><strong>To get the most from free tagging in collections:</strong></p>
+
+    <ul>
+      <li>Use specific, granular feed URLs.</li>
+      <li>Add feeds directly from <a class="ffun-normal-link"  href="#" @click.prevent="goToCollections()">collections</a> instead of using URLs from sites.</li>
+    </ul>
+
   </div>
 </template>
 
