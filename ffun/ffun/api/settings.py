44643eb0821cc67b695fa78b02f0a45edf3c730f
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-12-13 13:06:39 +0100

    continue development

--- ffun/ffun/api/settings.py
+++ ffun/ffun/api/settings.py
@@ -6,6 +6,7 @@ from ffun.core.settings import BaseSettings
 class Settings(BaseSettings):
     max_returned_entries: int = 10000
     max_feeds_suggestions_for_site: int = 100
+    max_entries_suggestions_for_site: int = 3
 
     model_config = pydantic_settings.SettingsConfigDict(env_prefix="FFUN_API_")
 
