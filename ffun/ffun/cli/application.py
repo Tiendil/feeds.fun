f015158d4ac313cbdf74b008d037c72e717fe7fd
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-12-27 14:43:45 +0100

    estimating entries per day for feed

--- ffun/ffun/cli/application.py
+++ ffun/ffun/cli/application.py
@@ -7,6 +7,7 @@ from ffun.cli.commands import metrics  # noqa: F401
 from ffun.cli.commands import processors_quality  # noqa: F401
 from ffun.cli.commands import profile  # noqa: F401
 from ffun.cli.commands import user_settings  # noqa: F401
+from ffun.cli.commands import estimates # noqa: F401
 from ffun.core import logging
 
 app = typer.Typer()
@@ -21,6 +22,7 @@ app.add_typer(metrics.cli_app, name="metrics")
 app.add_typer(profile.cli_app, name="profile")
 app.add_typer(experiments.cli_app, name="experiments")
 app.add_typer(fixtures.cli_app, name="fixtures")
+app.add_typer(estimates.cli_app, name="estimates")
 
 
 if __name__ == "__main__":
