54936a68280dd083ec30837539d2334f97a6f08e
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-11 17:22:57 +0100

    formatting

--- site/src/components/FfunTag.vue
+++ site/src/components/FfunTag.vue
@@ -28,87 +28,83 @@
 </template>
 
 <script lang="ts" setup>
-    import * as t from "@/logic/types";
-    import {computed, ref, inject} from "vue";
-    import type {Ref} from "vue";
-    import {useTagsStore} from "@/stores/tags";
-    import * as tagsFilterState from "@/logic/tagsFilterState";
+  import * as t from "@/logic/types";
+  import {computed, ref, inject} from "vue";
+  import type {Ref} from "vue";
+  import {useTagsStore} from "@/stores/tags";
+  import * as tagsFilterState from "@/logic/tagsFilterState";
   import * as asserts from "@/logic/asserts";
   import * as events from "@/logic/events";
 
-    const tagsStore = useTagsStore();
+  const tagsStore = useTagsStore();
 
-    const tagsStates = inject<Ref<tagsFilterState.Storage>>("tagsStates");
+  const tagsStates = inject<Ref<tagsFilterState.Storage>>("tagsStates");
 
+  asserts.defined(tagsStates);
+
+  const properties = defineProps<{
+    uid: string;
+    count?: number | null;
+    countMode?: string | null;
+    secondaryMode?: string | null;
+    showSwitch?: boolean | null;
+    changeSource: events.TagChangeSource;
+  }>();
+
+  const tagInfo = computed(() => {
+    const tagInfo = tagsStore.tags[properties.uid];
+
+    if (tagInfo) {
+      return tagInfo;
+    }
+
+    tagsStore.requestTagInfo({tagUid: properties.uid});
+
+    return t.noInfoTag(properties.uid);
+  });
+
+  const classes = computed(() => {
+    const result: {[key: string]: boolean} = {
+      tag: true
+    };
+
+    if (tagsStates.value.requiredTags[properties.uid]) {
+      result["required"] = true;
+    }
+
+    if (tagsStates.value.excludedTags[properties.uid]) {
+      result["excluded"] = true;
+    }
+
+    if (properties.secondaryMode) {
+      result[properties.secondaryMode] = true;
+    }
+
+    return result;
+  });
+
+  function onClick() {
     asserts.defined(tagsStates);
 
-    const properties = defineProps<{
-      uid: string;
-      count?: number | null;
-      countMode?: string | null;
-      secondaryMode?: string | null;
-      showSwitch?: boolean | null;
-      changeSource: events.TagChangeSource;
-    }>();
+    let changeInfo = tagsStates.value.onTagClicked({tag: properties.uid});
 
-    const tagInfo = computed(() => {
-      const tagInfo = tagsStore.tags[properties.uid];
+    await events.tagStateChanged({tag: properties.uid, source: properties.changeSource, ...changeInfo});
+  }
 
-      if (tagInfo) {
-        return tagInfo;
-      }
+  function onRevers() {
+    asserts.defined(tagsStates);
 
-      tagsStore.requestTagInfo({tagUid: properties.uid});
+    let changeInfo = tagsStates.value.onTagReversed({tag: properties.uid});
 
-      return t.noInfoTag(properties.uid);
-    });
+    await events.tagStateChanged({tag: properties.uid, source: properties.changeSource, ...changeInfo});
+  }
 
-    const classes = computed(() => {
-      const result: {[key: string]: boolean} = {
-        tag: true
-      };
-
-      if (tagsStates.value.requiredTags[properties.uid]) {
-        result["required"] = true;
-      }
-
-      if (tagsStates.value.excludedTags[properties.uid]) {
-        result["excluded"] = true;
-      }
-
-      if (properties.secondaryMode) {
-        result[properties.secondaryMode] = true;
-      }
-
-      return result;
-    });
-
-    function onClick() {
-      asserts.defined(tagsStates);
-
-      let changeInfo = tagsStates.value.onTagClicked({tag: properties.uid});
-
-      await events.tagStateChanged({tag: properties.uid,
-                                    source: properties.changeSource,
-                                    ...changeInfo});
+  const tooltip = computed(() => {
+    if (properties.countMode == "tooltip" && properties.count) {
+      return `articles with this tag: ${properties.count}`;
     }
-
-    function onRevers() {
-      asserts.defined(tagsStates);
-
-      let changeInfo = tagsStates.value.onTagReversed({tag: properties.uid});
-
-      await events.tagStateChanged({tag: properties.uid,
-                                    source: properties.changeSource,
-                                    ...changeInfo});
-    }
-
-    const tooltip = computed(() => {
-      if (properties.countMode == "tooltip" && properties.count) {
-        return `articles with this tag: ${properties.count}`;
-      }
-      return "";
-    });
+    return "";
+  });
 </script>
 
 <style scoped>
