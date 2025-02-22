a0e4e982b2f5daef9297aabd6f5bff1bc3f44c3c
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-22 18:40:16 +0100

    fixed create rule invitation

--- site/src/components/notifications/CreateRuleHelp.vue
+++ site/src/components/notifications/CreateRuleHelp.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="ffun-info-attention">
-    <h3>Create your first news score rule</h3>
-    <p>Set up a rule to filter your news:</p>
+  <div class="ffun-info-common">
+    <h4>Create your first sorting rule</h4>
+
     <ol class="list-decimal list-inside">
-      <li><strong>Click</strong> any tag on a news item.</li>
-      <li>See how news is filtered by the chosen tag.</li>
-      <li>Locate the <strong>Create Rule</strong> panel in the left sidebar.</li>
+      <li><strong>Click on a tag</strong>, that is intersting for you.</li>
+      <li>See how Feeds Fun filters news by the chosen tag.</li>
       <li>
-        Assign a numeric score for filtered news:
+        <strong>Assign a score</strong> for the filtered news in the "Create Rule" block in the left sidebar:
         <ul>
-          <li>Use higher scores (above zero) for important or engaging content.</li>
-          <li>Use lower (below zero) scores for uninteresting or irrelevant content.</li>
+          <li>Use scores above zero for important or engaging content.</li>
+          <li>Use scores below zero for irrelevant or dull content.</li>
         </ul>
       </li>
-      <li>Click <strong>Create Rule</strong>.</li>
-      <li>Watch the news reorganize based on your scores.</li>
+      <li><strong>Click "Create Rule" button</strong>.</li>
+      <li>See how Feeds Fun reorganize news based on the rule.</li>
     </ol>
+
   </div>
+
 </template>
