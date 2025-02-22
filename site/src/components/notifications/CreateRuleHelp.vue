0072a9d0352d7406ae179f9dbed37da33b390e41
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-22 18:43:09 +0100

    better notification

--- site/src/components/notifications/CreateRuleHelp.vue
+++ site/src/components/notifications/CreateRuleHelp.vue
@@ -3,17 +3,17 @@
     <h4>Create your first sorting rule</h4>
 
     <ol class="list-decimal list-inside">
-      <li><strong>Click on a tag</strong>, that is intersting for you.</li>
-      <li>See how Feeds Fun filters news by the chosen tag.</li>
+      <li><strong>Click on a tag</strong> that interests you.</li>
+      <li>See how Feeds Fun filters news based on the selected tag.</li>
       <li>
-        <strong>Assign a score</strong> for the filtered news in the "Create Rule" block in the left sidebar:
+        <strong>Assign a score</strong> to the filtered news in the "Create Rule" block in the left sidebar:
         <ul>
-          <li>Use scores above zero for important or engaging content.</li>
-          <li>Use scores below zero for irrelevant or dull content.</li>
+          <li>Use positive scores for important or engaging content.</li>
+          <li>Use negative scores for irrelevant or boring content.</li>
         </ul>
       </li>
-      <li><strong>Click "Create Rule" button</strong>.</li>
-      <li>See how Feeds Fun reorganize news based on the rule.</li>
+      <li><strong>Click the "Create Rule" button</strong>.</li>
+      <li>See how Feeds Fun reorganizes news based on your rule.</li>
     </ol>
 
   </div>
