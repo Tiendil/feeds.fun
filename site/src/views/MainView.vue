a2fc00f6dfc1eb24d20cdd6aae53b44c98c42163
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-25 18:13:26 +0100

    styling

--- site/src/views/MainView.vue
+++ site/src/views/MainView.vue
@@ -78,7 +78,36 @@
 
           <template #description>
 
-            <div class="grid grid-cols-2">
+            <div class="grid grid-cols-2 justify-items-start">
+
+              <div class="">
+
+                <fake-tag
+                  uid="sci-fi"
+                  name="sci-fi"
+                  link="http://example.com"
+                  css-modifier="positive" />
+
+                <i class="ti ti-arrow-right"></i>
+
+                <span class="cursor-pointer text-purple-700 text-xl">+5</span>
+
+              </div>
+
+              <div class="">
+
+                <fake-tag
+                  uid="news-dot-fake"
+                  name="news.fake"
+                  link="http://example.com"
+                  css-modifier="negative" />
+
+                <i class="ti ti-arrow-right"></i>
+
+                <span class="cursor-pointer text-purple-700 text-xl">-34</span>
+
+              </div>
+
               <div class="">
                 <fake-tag
                   uid="ufo"
@@ -101,45 +130,9 @@
               </div>
 
               <div class="">
-
-                <fake-tag
-                  uid="news-dot-fake"
-                  name="news.fake"
-                  link="http://example.com"
-                  css-modifier="negative" />
-
-                <i class="ti ti-arrow-right"></i>
-
-                <span class="cursor-pointer text-purple-700 text-xl">-21</span>
-
-              </div>
-
-              <div class="">
-
-                <fake-tag
-                  uid="sci-fi"
-                  name="sci-fi"
-                  link="http://example.com"
-                  css-modifier="positive" />
-
-                <i class="ti ti-arrow-right"></i>
-
-                <span class="cursor-pointer text-purple-700 text-xl">+5</span>
-
-              </div>
-
-              <div class="">
                 <fake-tag
                   uid="space-exploration"
                   name="space-exploration"
-                  :link="null"
-                  css-modifier="positive" />
-
-                <i class="ti ti-plus"></i>
-
-                <fake-tag
-                  uid="mars"
-                  name="mars"
                   :link="null"
                   css-modifier="positive" />
 
