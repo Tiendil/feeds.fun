7d3595819562d89c904e68b36dadc5c79ddb8d6c
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-26 15:18:36 +0100

    track front event without user id

--- ffun/ffun/api/http_handlers.py
+++ ffun/ffun/api/http_handlers.py
@@ -8,12 +8,13 @@ from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse
 
 from ffun.api import entities
 from ffun.api.settings import settings
-from ffun.auth.dependencies import User
+from ffun.auth.dependencies import User, OptionalUser
 from ffun.core import logging
 from ffun.core.api import Message, MessageType
 from ffun.core.errors import APIError
 from ffun.domain.entities import UserId
 from ffun.domain.urls import url_to_uid
+from ffun.domain.domain import no_user_id
 from ffun.feeds import domain as f_domain
 from ffun.feeds_collections.collections import collections
 from ffun.feeds_discoverer import domain as fd_domain
@@ -447,11 +448,13 @@ async def api_set_user_setting(request: entities.SetUserSettingRequest, user: Us
 
 
 @router.post("/api/track-event")
-async def api_track_event(request: entities.TrackEventRequest, user: User) -> entities.TrackEventResponse:
+async def api_track_event(request: entities.TrackEventRequest, user: OptionalUser) -> entities.TrackEventResponse:
     attributes = request.event.model_dump()
     event = attributes.pop("name")
 
-    logger.business_event(event, user_id=user.id, **attributes)
+    user_id = user.id if user is not None else no_user_id()
+
+    logger.business_event(event, user_id=user_id, **attributes)
 
     return entities.TrackEventResponse()
 
--- ffun/ffun/domain/domain.py
+++ ffun/ffun/domain/domain.py
@@ -3,6 +3,10 @@ import uuid
 from ffun.domain.entities import CollectionId, EntryId, FeedId, RuleId, SourceId, UserId
 
 
+def no_user_id() -> UserId:
+    return UserId(uuid.UUID(int=0))
+
+
 def new_user_id() -> UserId:
     return UserId(uuid.uuid4())
 
