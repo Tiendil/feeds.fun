217cd861863c61dd2afa217c243d4749c0d0a670
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-02-28 13:10:25 +0100

    ff-225 slightly bigger vertical spacing on the main page on mobile devices

--- site/src/components/main/Block.vue
+++ site/src/components/main/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center max-w-3xl mx-auto my-3 md:my-5 space-y-3 md:space-y-5">
+  <div class="text-center max-w-3xl mx-auto my-5 space-y-5">
     <slot></slot>
   </div>
 </template>
