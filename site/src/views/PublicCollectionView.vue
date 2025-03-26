1aefd0ac13fd290be874cd8c29dec6ccdb87daa9
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-03-26 17:40:02 +0100

    better intro

--- site/src/views/PublicCollectionView.vue
+++ site/src/views/PublicCollectionView.vue
@@ -77,6 +77,10 @@
           If you like the results, <a href="#" @click.prevent="router.push({name: 'main'})">register</a> to create your own scoring rules and automatically prioritize the news you’re most interested in.
         </p>
 
+        <p>
+          You can find more about scoring rules on the <a :href="router.resolve({name: 'main', params: {}}).href">main page</a>.
+        </p>
+
         <p><strong>All news in this collection is always up-to-date and tagged.</strong>
         </p>
 
