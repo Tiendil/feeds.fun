0bcc0110d683358db436a088611346002076d5bb
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-03-27 10:45:55 +0100

    removed todo

--- site/src/components/SupertokensLogin.vue
+++ site/src/components/SupertokensLogin.vue
@@ -9,7 +9,6 @@
   </div>
 
   <div v-else>
-    <!-- TODO: do we need h2 here? Check public collections view & main view-->
     <h2 class="my-0">Single e-mail to log in</h2>
     <template v-if="!requested">
       <p class="text-center">We'll send you an email with a login link, no password required.</p>
