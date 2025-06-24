080af0022c7a9ac0b36278c82c4bc14b3f8da505
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-06-24 15:25:53 +0200

    fix

--- ffun/ffun/loader/operations.py
+++ ffun/ffun/loader/operations.py
@@ -35,7 +35,7 @@ async def load_content(  # noqa: CFQ001, CCR001, C901 # pylint: disable=R0912, R
         headers = {"user-agent": user_agent, "accept-encoding": "br;q=1.0, gzip;q=0.9, deflate;q=0.8"}
 
         async with semaphore:
-            async with httpx.AsyncClient(proxies=proxy.url, headers=headers) as client:
+            async with httpx.AsyncClient(proxy=proxy.url, headers=headers) as client:
                 response = await client.get(url, follow_redirects=True)
 
     except httpx.RemoteProtocolError as e:
