1e1f81542a839cb11af3af577c398664124a9ad9
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-09-27 14:52:01 +0200

    wip

--- ffun/ffun/tags/normalizers/form_normalizer.py
+++ ffun/ffun/tags/normalizers/form_normalizer.py
@@ -20,138 +20,33 @@ def ensure_model(name: str,
         return spacy.load(name, disable=disable)
 
 
-# TODO: how could we reuse memory between normalizer runs?
-class Solution:
-    __slots__ = ('_base_vector',
-                 '_nlp',
-                 '_vectors',
-                 '_parts',
-                 '_alpha',
-                 '_beta',
-                 '_alpha_score',
-                 '_full_beta_score',
-                 '_beta_score',
-                 '_full_vector',
-                 '_score',
-                 )
+# TODO: we should periodically trim some caches
+class Cache:
+    __slots__ = ('_singular_cache', '_plural_cache', '_spacy_data', '_spacy_normal_cache', '_nlp')
 
-    def __init__(self,
-                 nlp: spacy.language.Language,
-                 base_vector: np.ndarray,
-                 alpha: float = 1.0,
-                 beta: float = 1.0
-                 ) -> None:
-        self._nlp = nlp
-        self._base_vector = base_vector
-        self._parts = []
-        self._vectors = {}
-        self._alpha = alpha
-        self._beta = beta
-        self._alpha_score = 0.0
-        self._full_beta_score = 0.0
-        self._beta_score = 0.0
-        self._full_vector = base_vector
-        self._score = 0.0
-
-    def _unit_vector(self, word: str) -> np.ndarray:
-        key = self._nlp.vocab.strings[word]
-
-        index = self._nlp.vocab.vectors.key2row.get(key, -1)
-
-        if index >= 0:
-            vector = self._nlp.vocab.vectors.data[index]
-        else:
-            vector = None
-
-        if vector is None or vector.shape[0] == 0:
-            return self._base_vector
-
-        norm = np.linalg.norm(vector)
-
-        if norm == 0.0:
-            return self._base_vector
-
-        return vector / norm
-
-    def unit_vector(self, word: str) -> np.ndarray:
-        if word in self._vectors:
-            return self._vectors[word]
-
-        vector = self._unit_vector(word)
-
-        self._vectors[word] = vector
-
-        return vector
-
-    def grow(self, part: str) -> 'Solution':
-        clone = Solution(nlp=self._nlp,
-                         base_vector=self._base_vector,
-                         alpha=self._alpha,
-                         beta=self._beta)
-        clone._parts = [part] + self._parts
-
-        # yes, we keep shared cache of vectors between solutions
-        clone._vectors = self._vectors
-
-        clone._full_vector = clone.unit_vector(part) + self._full_vector
-
-        if len(clone._parts) > 1:
-            clone._alpha_score = np.dot(clone._full_vector, self.unit_vector(clone._parts[-1])) / len(clone._parts)
-
-        if len(clone._parts) > 2:
-            clone._full_beta_score += np.dot(clone.unit_vector(clone._parts[0]),
-                                             clone.unit_vector(clone._parts[1]))
-
-            clone._beta_score = clone._full_beta_score / (len(clone._parts) - 1)
-
-        clone._score = clone._alpha * clone._alpha_score + clone._beta * clone._beta_score
-
-        return clone
-
-
-# TODO: comment/mark application of each guard
-# TODO: maybe organize guards in a lists, if it makes sense
-# TODO: tests
-# TODO: test performance
-# TODO: add to configs
-# TODO: add guard for numbers
-class Normalizer(base.Normalizer):
-    """Normalizes forms of tag parts.
-
-    Currently normalizes only time, but can be extended in the future.
-
-    The algorithm:
-
-    - Place the last part of the tag in singular form (if possible).
-    - For each other part of the tag, starting from the right:
-      - Let's imaging we have tag @a-b-tail
-      - Create two tag candidates: `@singular(b)-tail` & `@plural(b)-tail`
-      - Chose the tag, that has the highest similarity with @c, new tag will become tail
-      - Now we normalize tag @a-tail (got to the next loop iteration)
-
-    We compute similarity using cosine distance between word vectors.
-
-    We singularize the last part of the tag because it singularization produces better results than pluralization:
-
-    - We keep consistent proper names like `@charles-darwin`, `@new-york`
-    - We do not make bad word forms like `informations`, `learnings`, etc.
-    - => We do not need to implement prosessing of multiple corner cases .
-    """
-
-    __slots__ = ('_singular_cache', '_plural_cache', '_nlp', '_base_vector')
-
-    def __init__(self) -> None:
-        # TODO: we should periodically trim caches
+    def __init__(self, nlp: spacy.language.Language) -> None:
         self._singular_cache: dict[str, tuple[str]] = {}
         self._plural_cache: dict[str, tuple[str]] = {}
 
-        # TODO: parametrize
-        # TODO: when to load model?
-        # TODO: do not forget about loading in both dev/prod docker containers
-        self._nlp = ensure_model("en_core_web_lg")
+        self._nlp = nlp
+        self._spacy_data = nlp.vocab.vectors.data
+        self._spacy_normal_cache = np.linalg.norm(self._spacy_data, axis=1)
 
-        self._base_vector = np.zeros(self._nlp.vocab.vectors_length, dtype=np.float32)
-        self._base_vector.setflags(write=False)
+        # hack to reduce branching (do not check for zero norm each time)
+        self._spacy_normal_cache[self._spacy_normal_cache == 0.0] = 1.0
+
+    def get_row_index(self, word: str) -> int:
+        # TODO: potential memory leak, since SpaCy inserts each new word into vocab
+        key = self._nlp.vocab.strings[word]
+        return self._nlp.vocab.vectors.key2row.get(key, -1)
+
+    def get_row_norm(self, index: int) -> float:
+        if index < 0:
+            return 1.0
+        return self._spacy_normal_cache[index]
+
+    def get_row_vector(self, index: int) -> np.ndarray:
+        return self._spacy_data[index]
 
     def _get_word_base_forms(self, word: str) -> tuple[str]:
         for upos in ('NOUN', 'VERB', 'ADJ', 'ADV'):
@@ -190,8 +85,138 @@ class Normalizer(base.Normalizer):
 
         return plural_forms
 
+
+
+# TODO: how could we reuse memory between normalizer runs?
+class Solution:
+    __slots__ = ('_base_vector',
+                 '_cache',
+                 '_nlp',
+                 '_parts',
+                 '_alpha',
+                 '_beta',
+                 '_sum_alpha_score',
+                 '_alpha_score',
+                 '_sum_beta_score',
+                 '_beta_score',
+                 '_full_vector',
+                 '_score',
+                 )
+
+    def __init__(self,
+                 nlp: spacy.language.Language,
+                 cache: Cache,
+                 base_vector: np.ndarray,
+                 alpha: float = 1.0,
+                 beta: float = 1.0
+                 ) -> None:
+        self._nlp = nlp
+        self._cache = cache
+        self._base_vector = base_vector
+        self._parts = []
+        self._alpha = alpha
+        self._beta = beta
+        self._sum_alpha_score = 0.0
+        self._alpha_score = 0.0
+        self._sum_beta_score = 0.0
+        self._beta_score = 0.0
+        self._full_vector = base_vector
+        self._score = 0.0
+
+    def _cos_rows(self, row_a: int, row_b: int) -> float:
+        if row_a < 0 or row_b < 0:
+            return 0.0
+
+        vector_a = self._cache.get_row_vector(row_a)
+        vector_b = self._cache.get_row_vector(row_b)
+
+        norm_a = self._cache.get_row_norm(row_a)
+        norm_b = self._cache.get_row_norm(row_b)
+
+        return float(vector_a @ vector_b) / (norm_a * norm_b)
+
+    def grow(self, part: str) -> 'Solution':
+        clone = Solution(nlp=self._nlp,
+                         cache=self._cache,
+                         base_vector=self._base_vector,
+                         alpha=self._alpha,
+                         beta=self._beta)
+        clone._parts = [part] + self._parts
+
+        len_ = len(clone._parts)
+
+        if len_ == 1:
+            return clone
+
+        last_index = clone._cache.get_row_index(clone._parts[-1])
+        new_index = clone._cache.get_row_index(part)
+
+        if len_ > 1 and new_index >= 0:
+            clone._sum_alpha_score += self._sum_alpha_score + clone._cos_rows(new_index, last_index)
+
+        # We have two approaches to treat unknown words (without vectors):
+        # 1. Penalize solution with them (current approach):
+        #    do not increase sum_(alpha/beta)_score, but increase len_
+        #    which decreases average alpha_score
+        # 2. Ignore them (calculate average only for known words).
+        clone._alpha_score = clone._sum_alpha_score / len_
+
+        if len_ > 2:
+            next_index = clone._cache.get_row_index(clone._parts[1])
+
+            if new_index >= 0:
+                clone._sum_beta_score += self._sum_beta_score + clone._cos_rows(new_index, next_index)
+                clone._beta_score = clone._sum_beta_score / (len_ - 1)
+
+        clone._score = clone._alpha * clone._alpha_score + clone._beta * clone._beta_score
+
+        return clone
+
+
+# TODO: comment/mark application of each guard
+# TODO: maybe organize guards in a lists, if it makes sense
+# TODO: tests
+# TODO: test performance
+# TODO: add to configs
+# TODO: add guard for numbers
+class Normalizer(base.Normalizer):
+    """Normalizes forms of tag parts.
+
+    Currently normalizes only time, but can be extended in the future.
+
+    The algorithm:
+
+    - Place the last part of the tag in singular form (if possible).
+    - For each other part of the tag, starting from the right:
+      - Let's imaging we have tag @a-b-tail
+      - Create two tag candidates: `@singular(b)-tail` & `@plural(b)-tail`
+      - Chose the tag, that has the highest similarity with @c, new tag will become tail
+      - Now we normalize tag @a-tail (got to the next loop iteration)
+
+    We compute similarity using cosine distance between word vectors.
+
+    We singularize the last part of the tag because it singularization produces better results than pluralization:
+
+    - We keep consistent proper names like `@charles-darwin`, `@new-york`
+    - We do not make bad word forms like `informations`, `learnings`, etc.
+    - => We do not need to implement prosessing of multiple corner cases .
+    """
+
+    __slots__ = ('_nlp', '_base_vector', '_cache')
+
+    def __init__(self) -> None:
+        # TODO: parametrize
+        # TODO: when to load model?
+        # TODO: do not forget about loading in both dev/prod docker containers
+        self._nlp = ensure_model("en_core_web_lg")
+
+        self._cache = Cache(nlp=self._nlp)
+
+        self._base_vector = np.zeros(self._nlp.vocab.vectors_length, dtype=np.float32)
+        self._base_vector.setflags(write=False)
+
     def get_main_tail_form(self, word: str) -> str:
-        base_forms = self.get_word_base_forms(word)
+        base_forms = self._cache.get_word_base_forms(word)
 
         if len(base_forms) == 1:
             return base_forms[0]
@@ -208,14 +233,14 @@ class Normalizer(base.Normalizer):
         if any(c.isdigit() for c in word):
             return [word]
 
-        base_forms = self.get_word_base_forms(word)
+        base_forms = self._cache.get_word_base_forms(word)
 
         # print('!base forms for', word, ':', base_forms)
 
         forms = list(base_forms)
 
         for base_form in base_forms:
-            for plural_form in self.get_word_plural_forms(base_form):
+            for plural_form in self._cache.get_word_plural_forms(base_form):
                 # print('!plural form for', base_form, ':', plural_form)
                 if plural_form not in forms:
                     forms.append(plural_form)
@@ -253,7 +278,9 @@ class Normalizer(base.Normalizer):
 
         # return False, []
 
-        solution = Solution(nlp=self._nlp, base_vector=self._base_vector).grow(last_part)
+        solution = Solution(nlp=self._nlp,
+                            cache=self._cache,
+                            base_vector=self._base_vector).grow(last_part)
 
         for part in reversed(tag.parts[:-1]):
             solution = self.choose_candidate_step(solution, part)
