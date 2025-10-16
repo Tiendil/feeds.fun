c174f20d53f4fa43b7f0dfd9fb73faa7475e7c51
Aliaksei Yaletski <a.eletsky@gmail.com>
2025-10-16 17:46:17 +0200

    ff-527 — Fixed a bug when the CLI command to run migrations (`ffun yoyo migrate`) did not take an exclusive lock on migrations to prevent concurrent runs. (#448)

--- changes/unreleased.md
+++ changes/unreleased.md
@@ -1,3 +1,4 @@
 
 - ff-530 — Fixed a crash when tags renormalization is running in parallel with tags removal.
 - ff-537 — Removed unnecessary alert dialog on sending login email.
+- ff-527 — Fixed a bug when the CLI command to run migrations (`ffun yoyo migrate`) did not take an exclusive lock on migrations to prevent concurrent runs.
--- ffun/ffun/core/migrations.py
+++ ffun/ffun/core/migrations.py
@@ -11,13 +11,15 @@ async def apply_all() -> None:
     migrations: MigrationList = read_migrations(f"{ffun_root}/*/migrations")
 
     with get_backend(app_settings.postgresql.dsn_yoyo, "yoyo_migrations") as backend:
-        migrations_to_apply: MigrationList = backend.to_apply(migrations)
-        backend.apply_migrations(migrations_to_apply)
+        with backend.lock():
+            migrations_to_apply: MigrationList = backend.to_apply(migrations)
+            backend.apply_migrations(migrations_to_apply)
 
 
 async def rollback_all() -> None:
     migrations: MigrationList = read_migrations(f"{ffun_root}/*/migrations")
 
     with get_backend(app_settings.postgresql.dsn_yoyo, "yoyo_migrations") as backend:
-        migrations_to_rollback: MigrationList = backend.to_rollback(migrations)
-        backend.rollback_migrations(migrations_to_rollback)
+        with backend.lock():
+            migrations_to_rollback: MigrationList = backend.to_rollback(migrations)
+            backend.rollback_migrations(migrations_to_rollback)
