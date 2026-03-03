0c9e0ffc7c9f366e6caefe19c3da13eaf9c4a6eb
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-03-03 18:00:15 +0100

    wip

--- changes/unreleased.md
+++ changes/unreleased.md
@@ -1,5 +1,6 @@
 ### Changes
 
+- ff-406 Added `ffun feeds unlink <feed_ids>` command to unlink specified feeds from all users.
 - ff-664 Improved type checking — `Any` type is disallowed (mostly).
   - Unified URLs parsing error handling.
   - Fixed parsing of URLs like `at://did:plc:<id>/app.bsky.actor.profile/self` (presented in Bluesky HTML).
--- ffun/ffun/cli/commands/feeds.py
+++ ffun/ffun/cli/commands/feeds.py
@@ -10,11 +10,11 @@ from ffun.feeds_links.domain import unlink_feeds_from_all_users
 cli_app = typer.Typer()
 
 
-async def run_remove(feed_ids: list[FeedId]) -> None:
+async def run_unlink(feed_ids: list[FeedId]) -> None:
     async with with_app():
         await unlink_feeds_from_all_users(feed_ids)
 
 
 @cli_app.command()  # type: ignore
-def remove(feed_ids: list[uuid.UUID]) -> None:
-    asyncio.run(run_remove([FeedId(feed_id) for feed_id in feed_ids]))
+def unlink(feed_ids: list[uuid.UUID]) -> None:
+    asyncio.run(run_unlink([FeedId(feed_id) for feed_id in feed_ids]))
